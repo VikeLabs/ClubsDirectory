ffcd15006a2af8eddd7cb9e3c89ad825e29a494c
Aomi <aomijokoji@gmail.com>
2021-07-14 21:59:14 -0700

    feat: implement mock Footer

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -3,8 +3,8 @@ import { Box, Flex } from '@chakra-ui/react';
 export function Footer(): JSX.Element {
   return (
     <Box as="footer" width="100%">
-      <Flex bg="#044259" minHeight="30px" maxH="88px"></Flex>
-      <Flex bg="#1b8091" minHeight="60px" px="8"></Flex>
+      <Flex bg="brand.secondary" minHeight="30px" maxH="88px"></Flex>
+      <Flex bg="brand.primary" minHeight="60px" px="8"></Flex>
     </Box>
   );
 }
