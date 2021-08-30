ae1145f9265e4a567b51ea6c1c104d1ddc97980b
Aomi <aomijokoji@gmail.com>
2021-08-29 23:50:12 -0700

    feat: interface for JSON

---
+++ src/JSON/ClubData.ts
@@ -0,0 +1,12 @@
+// ClubData is to be used with the JSON data (casting)
+export interface ClubData {
+  id: string;
+  name: string;
+  category: string;
+  primaryEmail: string;
+  socialMedia: string[];
+  members: string;
+  tags: string[];
+  description: string;
+  createdDate: string;
+}
