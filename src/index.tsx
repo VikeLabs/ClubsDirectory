c77f25543b14442299af5179b0a0ceaf8f98c448
Aomi <aomijokoji@gmail.com>
2021-09-08 22:13:37 -0700

    feat: add theme typing

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,10 @@
-import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import { ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './Assets/CSS/index.css';
 import App from './App';
-
-const uvssTheme = extendTheme({
-  colors: {
-    brand: {
-      primary: '#1b8091',
-      secondary: '#044259',
-    },
-  },
-});
+import { uvssTheme } from './theme';
 
 ReactDOM.render(
   <React.StrictMode>
---
+++ src/theme.ts
@@ -0,0 +1,10 @@
+import { extendTheme } from '@chakra-ui/react';
+
+export const uvssTheme = extendTheme({
+  colors: {
+    brand: {
+      primary: '#1b8091',
+      secondary: '#044259',
+    },
+  },
+});
