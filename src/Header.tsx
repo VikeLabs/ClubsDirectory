3d4f0890cb3e2b6478246f9a2fb9abd06d684249
Aomi <aomijokoji@gmail.com>
2021-09-08 23:59:28 -0700

    feat: add uvss logo and remove header text

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,29 +1,22 @@
-import { Box, Flex, Heading, Text, useMediaQuery } from '@chakra-ui/react';
-import { AiOutlineSearch } from 'react-icons/ai';
-import { GiHamburgerMenu } from 'react-icons/gi';
+import { Box, Flex, LinkBox } from '@chakra-ui/react';
 
-const headingTitles = ['ABOUT', 'EVENTS', 'HEALTH & DENTAL', 'RESOURCES', 'SERVICES', 'JOBS', 'CONTACT'];
+import { ReactComponent as Logo } from './uvss.svg';
 
 // Implements a mocked-out version of the header on the UVSS homepage. Not meant to be a 1:1 replica
 export function Header(): JSX.Element {
-  const [isMobile] = useMediaQuery('(min-width: 990px)');
   return (
     <Box as="header" position="fixed" top="0" width="100%" maxWidth="1130px" boxShadow="md" zIndex="1">
       <Flex bg="brand.secondary" minHeight="30px" alignItems="center"></Flex>
-      <Flex bg="brand.primary" minHeight="60px" alignItems="center" justifyContent="space-between" px="8">
-        <Heading color="white">UVSS</Heading>
-        {isMobile ? (
-          <Flex as="nav" alignItems="center">
-            {headingTitles.map((title) => (
-              <Text color="white" key={title} mx="1.5">
-                {title}
-              </Text>
-            ))}
-            <AiOutlineSearch color="white" />
-          </Flex>
-        ) : (
-          <GiHamburgerMenu color="white" size="2em" />
-        )}
+      <Flex
+        bg="brand.primary"
+        minHeight="60px"
+        alignItems="center"
+        justifyContent="space-between"
+        pl={['2', '6', '12']}
+      >
+        <LinkBox as="a" href="https://uvss.ca">
+          <Logo height="50px" />
+        </LinkBox>
       </Flex>
     </Box>
   );
