456acd6a73f5df5afcfe0be2f006d77da801e4a3
Aomi <aomijokoji@gmail.com>
2021-09-12 19:50:51 -0700

    feat: add breakpoints to theme

--- src/theme.ts
+++ src/theme.ts
@@ -1,10 +1,20 @@
 import { extendTheme } from '@chakra-ui/react';
+import { createBreakpoints } from '@chakra-ui/theme-tools';
+
+const breakpoints = createBreakpoints({
+  sm: '420px',
+  md: '768px',
+  lg: '960px',
+  xl: '1130px',
+});
 
 export const uvssTheme = extendTheme({
   colors: {
     brand: {
       primary: '#1b8091',
       secondary: '#044259',
+      light: '#ddf0f1',
     },
   },
+  breakpoints,
 });
