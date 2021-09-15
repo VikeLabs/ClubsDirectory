32def348271a5428dc087a3f3c997989ac326b63
Aomi <aomijokoji@gmail.com>
2021-09-14 22:22:29 -0700

    fix: remove remaining ng prefix

--- src/links.ts
+++ src/links.ts
@@ -1,8 +1,8 @@
 export function generateCategoryLink(category: string): string {
-  return `/ng/categories/${category}`;
+  return `/categories/${category}`;
 }
 
 // called "Group" since it imcludes both "Clubs" and "Course Unions" etc.
 export function generateGroupLink(category: string, slug: string): string {
-  return `/ng/categories/${category}/${slug}`;
+  return `/categories/${category}/${slug}`;
 }
