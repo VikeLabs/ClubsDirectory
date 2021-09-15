0f62bb3761e05a7093ca4a40d8c6bfc1e24ff545
Aomi <aomijokoji@gmail.com>
2021-09-14 22:15:54 -0700

    feat: remove ng prefix

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 // TODO: remove this
 import './Assets/CSS/GlobalBackgroundImage.css';
-import ClubInfoPage from './ClubInfo/ClubInfoPage';
 import { CategoryPage } from './ClubsCategories/CategoryPage';
-import ClubListPage from './ClubsList/ClubListPage';
 import { Footer } from './Footer';
 import { Header } from './Header';
 import { ClubPage } from './pages/club';
@@ -24,11 +22,8 @@ function App() {
             {/* TODO: remove when link on UVSS website is changed to /categories */}
             <Route exact path="/ClubCategories" component={CategoryPage} />
             <Route exact path="/categories" component={CategoryPage} />
-            <Route exact path="/categories/:category" component={ClubListPage} />
-            <Route exact path="/categories/:category/:slug" component={ClubInfoPage} />
-            {/* WIP */}
-            <Route exact path="/ng/categories/:category" component={ClubsPage} />
-            <Route exact path="/ng/categories/:category/:slug" component={ClubPage} />
+            <Route exact path="/categories/:category" component={ClubsPage} />
+            <Route exact path="/categories/:category/:slug" component={ClubPage} />
           </Box>
         </Switch>
       </Router>
